2c289a8c84821f985d3563c3ad3679e994329507
Dotun Olusoga <adedotun.olusoga@gmail.com>
2015-01-22 15:58:37 -0600

    compartmentalized the program using the CommonJS module system

--- app.js
+++ app.js
@@ -1,95 +1,11 @@
-var _ = require('lodash');
+'use strict';
 
-function Game(numberOfPlayers){
-  this.weapons = [];
-  this.weapons.push(new Weapon({name: 'Sword', damage: 0.2, ammo: Infinity}));
-  this.weapons.push(new Weapon({name: 'Shotgun', damage: 0.6, ammo: 1}));
-  this.weapons.push(new Weapon({name: 'Fist', damage: 0.05, ammo: Infinity}));
-  this.players = [];
+var game,
+    Game = require('./lib/Game');
 
-  for (var i=0; i < numberOfPlayers; i++){
-    var player = new Player({name: 'Player' + (i + 1)});
-    player.weapon = getRandomWeapon(this.weapons);
-    this.players.push(player);
-  }
-
-  function getRandomWeapon(weaponList){
-    return weaponList[getRandomInt(0, weaponList.length)];
-  }
-
-  function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
-}
-
-function Player(obj){
-  this.name = obj && obj.name || 'Player';
-  this.health = 1;
-  this.isZombie = false;
-  this.isTrulyDead = false;
-  this.dexterity = 0.5;
-}
-
-Player.prototype.attack = function(otherPlayer){
-  if (didHit(this.weapon.accuracy)) {
-    otherPlayer.health -= this.weapon.damage;
-  }
-};
-
-function Weapon(obj){
-  this.name = obj.name;
-  this.damage = obj.damage;
-  this.accuracy = 1 - obj.damage;
-  this.ammo = obj && obj.ammo || 0;
-}
-
-function didHit(accuracy){
-  return accuracy > Math.random();
-}
-
-Game.prototype.fight = function(){
-  this.alivePlayers()
-    .forEach(function(player, i, alivePlayersArray){
-      var playerIndex = alivePlayersArray.indexOf(player);
-      console.log(playerIndex);
-
-      if (playerIndex !== -1) {
-        var nextPlayer = alivePlayersArray[playerIndex+1] || alivePlayersArray[0];
-        player.attack(nextPlayer);
-      }
-  });
-
-  return this;
-};
-
-Game.prototype.shufflePlayerOrder = function(){
-  this.players = _.shuffle(this.players);
-  return this;
-};
-
-Game.prototype.shuffleAndFight = function(){
-  return this
-    .shufflePlayerOrder()
-    .fight();
-};
-
-Game.prototype.alivePlayers = function(){
-  return this.players.filter(function(player){
-    return Math.round(player.health * 1e4)/1e4 >= 0;
-  });
-};
-
-Game.prototype.fightToTheDeath = function(){
-  while (this.aliveplayers().length > 1){
-    this.shuffleAndFight();
-  }
-};
-
-var game = new Game(4);
-game
-  .shuffleAndFight()
-  .shuffleAndFight()
-  .shuffleAndFight()
-  .shuffleAndFight();
+game = new Game(4);
+game.fightToTheDeath();
 
 console.log(JSON.stringify(game, null, 2));
+
+
